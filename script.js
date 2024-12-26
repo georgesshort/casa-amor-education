5fa5af74d4d0182149e17d9e36805e16ff296a21
georgesshort <george@sidneygeorge.com>
2024-12-26 14:08:16 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function uploadLesson() {
+  alert("Feature coming soon: Upload lesson materials!");
+}
+
+function playFlashcards() {
+  alert("Flashcards will be added soon. Stay tuned!");
+}
+
+function playBingo() {
+  alert("Bingo game is in development!");
+}
